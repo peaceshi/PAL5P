2f1c591e7a9c6e0666b805341a358fb414880dc0
peaceshi <skyflyshp@qq.com>
2017-10-12 07:11:57 +0800

    大量重构代码

--- xml_parser.py
+++ xml_parser.py
@@ -1,62 +1,44 @@
 import xmltodict
-import os,sys
-
-file_path = sys.path[0] + "/Data/weapon.xml"
-
-print(file_path)
-
-with open(file_path) as xml_document:
-    doc = xmltodict.parse(xml_document.read())
-'''
-def list_all_dict(dict_a):
-    if isinstance(dict_a, dict) : #使用isinstance检测数据类型
-        for x in range(len(dict_a)):
-            temp_key = list(dict_a.keys())[x]
-            temp_value = dict_a[temp_key]
-            print("%s : %s", temp_key, temp_value)
-            list_all_dict(temp_value) #自我调用实现无限遍历
+from file_paths import *
 
 
-list_all_dict(doc)
-'''
-
-# 遍历生成的字典
-for root_index in range(len(doc)):
-    node_name = list(doc.keys())[root_index]
-    node_value = doc[node_name]
-    leaf_value = []
-    item_used_number_sets = []  # 已使用的编号
-    if root_index == 0:
-        root_node_name = node_name
-        root_node_value = node_value
-        print("根节点名称: ", root_node_name)
-
-    while True:   # 不是叶子节点全是dict
-        if isinstance(node_value, dict):
-            tree_node_name = list(node_value.keys())[0]
-            tree_node_value = node_value[tree_node_name]
-            print("子节点名称: ", tree_node_name)
-            if isinstance(tree_node_value, dict):
-                node_value = tree_node_value
-            else:
-                leaf_value = tree_node_value
-                break
+def load_xml_document_files(files):
+    for file_index, file_name in enumerate(files):
+        file_path = sys.path[0] + "/Data/" + file_name
+        # print(file_path)
+        if os.access(file_path, os.R_OK):
+            with open(file_path) as xml_document:
+                traverse_dict(xmltodict.parse(xml_document.read()))
         else:
-            leaf_value = node_value
-            break
+            print("failed to access file.", file_path)
+            sys.exit(1)
+# load_xml_document_files(files)
 
+
+def traverse_dict(dictionary):  # 遍历生成的字典
+    if isinstance(dictionary, dict):
+        for node_index in range(len(dictionary)):
+            node_name = list(dictionary.keys())[node_index]
+            node_value = dictionary[node_name]
+            traverse_dict(node_value)
+    else:
+        leaf(dictionary)
+# traverse_dict(dictionary)
+
+
+def leaf(leaf_value):
+    item_used_number_sets = []  # 本类型中已使用的编号
     for tree_index, tree_node in enumerate(leaf_value):  # 叶节点是list
         dict_key_number = len(tree_node)
         item_value = []  # 项目值
         item_attribute = []  # 项目属性/名称
-
         for leaf_index in range(dict_key_number):
             leaf_node_name = list(tree_node.keys())[leaf_index]  # 这个list包含若干个dict
             leaf_node_value = tree_node[leaf_node_name]
             if tree_index == 0:
                 item_attribute.append(leaf_node_name)
-            if leaf_index == 0:  # 只要编号
-                item_used_number_sets.append(int(leaf_node_value))
+            if leaf_index == 0:  # 只要编号项
+                item_used_number_sets.append(leaf_node_value)
             item_value.append(leaf_node_value)
             if dict_key_number <= leaf_index + 1:  # 是否遍历了所有叶子节点
                 for item_attribute_index, attribute_value in enumerate(item_attribute):
@@ -65,8 +47,7 @@ for root_index in range(len(doc)):
                 for item_value_index, value_value in enumerate(item_value):
                     print(value_value, end=';', flush=True)
                 print('')
-for index, value in enumerate(item_used_number_sets):
-    print(value, index)
-
-print("process shutdown")
+    for item_used_number_sets_index, value in enumerate(item_used_number_sets):
+        print(value, item_used_number_sets_index)
+# leaf()
 
