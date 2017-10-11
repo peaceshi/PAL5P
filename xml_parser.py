8faccf2911e74bfdcd92e5a5c285036b33c30791
peaceshi <skyflyshp@qq.com>
2017-10-12 03:10:40 +0800

    添加已使用编号查询和部分注释

--- xml_parser.py
+++ xml_parser.py
@@ -1,7 +1,7 @@
 import xmltodict
 import os,sys
 
-file_path = sys.path[0] + "/Data/skill.xml"
+file_path = sys.path[0] + "/Data/weapon.xml"
 
 print(file_path)
 
@@ -19,10 +19,13 @@ def list_all_dict(dict_a):
 
 list_all_dict(doc)
 '''
+
+# 遍历生成的字典
 for root_index in range(len(doc)):
     node_name = list(doc.keys())[root_index]
     node_value = doc[node_name]
     leaf_value = []
+    item_used_number_sets = []  # 已使用的编号
     if root_index == 0:
         root_node_name = node_name
         root_node_value = node_value
@@ -44,22 +47,26 @@ for root_index in range(len(doc)):
 
     for tree_index, tree_node in enumerate(leaf_value):  # 叶节点是list
         dict_key_number = len(tree_node)
-        item_value = []
-        item_attribute = []
+        item_value = []  # 项目值
+        item_attribute = []  # 项目属性/名称
+
         for leaf_index in range(dict_key_number):
-            leaf_node_name = list(tree_node.keys())[leaf_index]  # 这个list若干个包含dict
+            leaf_node_name = list(tree_node.keys())[leaf_index]  # 这个list包含若干个dict
             leaf_node_value = tree_node[leaf_node_name]
             if tree_index == 0:
                 item_attribute.append(leaf_node_name)
+            if leaf_index == 0:  # 只要编号
+                item_used_number_sets.append(int(leaf_node_value))
             item_value.append(leaf_node_value)
-           # print("叶子节点名称: ", leaf_node_name[1:], " 叶子节点值: ", leaf_node_value)
-            if dict_key_number <= leaf_index + 1:
+            if dict_key_number <= leaf_index + 1:  # 是否遍历了所有叶子节点
                 for item_attribute_index, attribute_value in enumerate(item_attribute):
                     print(attribute_value[1:], end=';', flush=True)
                 print('')
                 for item_value_index, value_value in enumerate(item_value):
                     print(value_value, end=';', flush=True)
                 print('')
+for index, value in enumerate(item_used_number_sets):
+    print(value, index)
 
 print("process shutdown")
 
